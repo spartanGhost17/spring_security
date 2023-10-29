b459c41a41e98298b473a01f29e1776dc6edf2d4
spartanGhost17 <adambaku5216@gmail.com>
2023-10-29 21:11:51 +0000

    added custom filter chain

--- springsecurity/src/main/java/com/learning/springsecurity/config/ProjectSecurityConfig.java
+++ springsecurity/src/main/java/com/learning/springsecurity/config/ProjectSecurityConfig.java
@@ -1,50 +1,30 @@
 package com.learning.springsecurity.config;
 
+import com.learning.springsecurity.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
-import javax.sql.DataSource;
-import java.util.ArrayList;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
 
     /**
-     * focuses on defining custom security configuration
-     *
-     * @param http
-     * @return
-     * @throws Exception
-     */
-    @Bean(value = "defaultSecurityFilterChain")
-    //@Order(2147483642)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests((requests) -> {
-            requests.requestMatchers("/api/v1/myAccount", "/balance/**", "/loans/**", "/cards/**", "/user").authenticated() //authenticated forces spring to secure those end-points
-                    .requestMatchers("/notices/**", "/contact/**", "/register").permitAll();
-        });
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain) http.build();
-    }
-
-    /**
      * determine password encoder
+     *
      * @return
      */
     @Bean(value = "passwordEncoder")
@@ -54,10 +34,73 @@ public class ProjectSecurityConfig {
 
 
     /**
+     * focuses on defining custom security configuration
+     * with disabled csrf and no cors
+     *
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean(value = "defaultSecurityFilterChain")
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        CsrfTokenRequestAttributeHandler requestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        requestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
+        //requireExplicitSave(false) gives spring all the responsability of generating and storing JSessionId in context
+        http.securityContext().requireExplicitSave(false)
+                //tell spring how to manage session using defined session management below
+                //basically tells spring to create a JSessionId after initial login is completed and persist that id
+                //UI will leverage that for all subsequent value
+                .and().sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));//actual prod domain
+                        configuration.setAllowedMethods(Collections.singletonList("*"));//Arrays.asList("GET", "POST", "PUT", "DELETE")
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));//Arrays.asList("Authorization", "Content-Type")
+                        configuration.setMaxAge(3600L);
+                        return configuration;
+                    }
+                }).and().csrf((csrf) -> csrf.csrfTokenRequestHandler(requestAttributeHandler).ignoringRequestMatchers("/contact/**", "/register").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())) //enforced for public post methods to prevent csrf attack only if method is not GET
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class) //execute CsrfCookieFilter after BasicAuthenticationFilter when login is complete and persist csrf token
+                .authorizeHttpRequests((requests) -> {
+                    requests.requestMatchers("/api/v1/myAccount", "/balance/**", "/loans/**", "/cards/**", "/user").authenticated() //authenticated forces spring to secure those end-points
+                            .requestMatchers("/notices/**","/register", "/contact/**").permitAll();
+                });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain) http.build();
+    }
+
+
+    /**
+     * focuses on defining custom security configuration
+     * with disabled csrf and no cors
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    //@Bean(value = "defaultSecurityFilterChain")
+    //@Order(2147483642)
+    /*SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests((requests) -> {
+            requests.requestMatchers("/api/v1/myAccount", "/balance/**", "/loans/**", "/cards/**", "/user").authenticated() //authenticated forces spring to secure those end-points
+                    .requestMatchers("/notices/**", "/contact/**", "/register").permitAll();
+        });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain) http.build();
+    }*/
+
+
+    /**
      * no encryption for password encoder
      * @Bean(value = "passwordEncoder")
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    return NoOpPasswordEncoder.getInstance();
     }*/
 
 
